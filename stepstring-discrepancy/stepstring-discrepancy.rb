2e0bef7a81f7d84e9457adcd12fe8721c51361c6
Caleb Astey <caleb.astey@opower.com>
2015-05-30 10:58:59 -0400

    Updated comments

--- stepstring-discrepancy/stepstring-discrepancy.rb
+++ stepstring-discrepancy/stepstring-discrepancy.rb
@@ -1,8 +1,18 @@
 ##
 # usage: ruby stepstring-discrepancy.rb input_string
 #
-# Status:  Working, DP?
+# Status:  Working
+#
+# Optimizations over brute force:
+#   * Uses Kadane's Algorithm to find the maximum subarray sum
+#   * Modifies step size so calculations aren't repeated
+#   * Only calculates the step string and discrepancy if the stepstring is > current max.
+#
+# Opimizations todo:
+#   * Modify Kadane's algorithm to get max subarray in a single pass
 
+
+#Kadane's Algorithm
 def max_subarray(vals)
   max = 0
   max_here = 0
@@ -14,7 +24,7 @@ def max_subarray(vals)
   max
 end
 
-#2n max subarray
+#Need to run Kadane's on both A and B.
 def discrepancy(str)
   a_mapping = { 'a' => 1, 'b' => -1}
   b_mapping = { 'a' => -1, 'b' => 1}
@@ -32,6 +42,7 @@ def max_discrepancy(str)
   (0...str.length).each do |i|
     max_step_size = str.length - i
     ((i + 1)..max_step_size).each do |j|
+      # No need to calculate if the stepstring is smaller than current max
       if ((str.length - i) / j) + 1 > max
         d = discrepancy(get_stepstring(str, i, j))
         max = d if d > max
